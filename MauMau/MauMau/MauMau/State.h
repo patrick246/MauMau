018e7bb0c39d053020feb634e10931dab4fd30ca
Patrick Hahn <patricksilashahn@gmx.net>
2013-08-08 00:23:58 +0200

    Added a reference to GameApp in the State class, the constructor of it now takes a reference to the GameApp instance

--- MauMau/MauMau/MauMau/State.h
+++ MauMau/MauMau/MauMau/State.h
@@ -1,12 +1,20 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 
+class GameApp;
 class State
 	:	sf::Drawable
 {
+	State(GameApp& app) 
+		:	m_app(app)
+	{}
+
 	// Virtual method for updating the state
 	virtual void update(float frametime) {}
 
 	// Virtual method for displaying the state
 	virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const {}
+
+protected:
+	GameApp& m_app;
 };
